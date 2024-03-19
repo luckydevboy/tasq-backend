f2acfcfe0deef924603f47ab4b7cf01ed14e7082
Mohammad Reza Ghasemi <mrghasemi1992@gmail.com>
2024-03-19 12:36:58 +0330

    feat: filter tasks (#6)

--- src/controllers/taskController.ts
+++ src/controllers/taskController.ts
@@ -4,7 +4,7 @@ import { Task } from "../models";
 import { decodeAuthToken } from "../utils";
 import handleErrors from "../utils/handleErrors";
 
-// export const getAllTasks = async (req: Request, res: Response) => {
+// export const getTasks = async (req: Request, res: Response) => {
 //   const page = parseInt(req.query.page as string) || 1;
 //   const pageSize = parseInt(req.query.pageSize as string) || 10;
 
@@ -30,11 +30,59 @@ import handleErrors from "../utils/handleErrors";
 //   }
 // };
 
-export const getAllTasks = async (req: Request, res: Response) => {
+type Filter = "completed" | "scheduled" | "deleted" | "today";
+
+interface QueryScheduled {
+  user: string | undefined;
+  dueDate?: { $exists: boolean };
+}
+
+interface QueryCompleted {
+  user: string | undefined;
+  completed?: boolean;
+}
+
+interface QueryDeleted {
+  user: string | undefined;
+  deleted?: boolean;
+}
+
+interface QueryToday {
+  user: string | undefined;
+  scheduledDate?: { $gte: number; $lt: number };
+}
+
+export const getTasks = async (req: Request, res: Response) => {
   try {
     const userId = decodeAuthToken(req)?.id;
+    const filter = req.params.filter as Filter;
 
-    const tasks = await Task.find({ user: userId }).sort({ createdAt: -1 });
+    let query: QueryScheduled | QueryCompleted | QueryDeleted | QueryToday;
+
+    switch (filter) {
+      case "scheduled":
+        query = { user: userId, dueDate: { $exists: true } };
+        break;
+      case "completed":
+        query = { user: userId, completed: true };
+        break;
+      case "deleted":
+        query = { user: userId, deleted: true };
+        break;
+      case "today":
+        query = {
+          user: userId,
+          scheduledDate: {
+            $gte: new Date().setHours(0, 0, 0, 0),
+            $lt: new Date().setHours(23, 59, 59, 999),
+          },
+        };
+        break;
+      default:
+        query = { user: userId };
+    }
+
+    const tasks = await Task.find(query).sort({ createdAt: -1 });
 
     return res.json({
       status: "success",
--- src/routes/taskRoutes.ts
+++ src/routes/taskRoutes.ts
@@ -1,18 +1,13 @@
 import express from "express";
 
 import { authorization } from "../middlewares";
-import {
-  createTask,
-  deleteTask,
-  getAllTasks,
-  updateTask,
-} from "../controllers";
+import { createTask, deleteTask, getTasks, updateTask } from "../controllers";
 
 const router = express.Router();
 
 router.use(authorization);
 
-router.get("/", getAllTasks);
+router.get("/:filter?", getTasks);
 router.post("/", createTask);
 router.delete("/:taskId", deleteTask);
 router.put("/:taskId", updateTask);
